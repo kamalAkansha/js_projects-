a736e57677ad04d727c48a17c87f92a80438b975
kamalAkansha <166831619+kamalAkansha@users.noreply.github.com>
2024-05-19 22:31:49 +0530

    Update hex.js

--- 1_color-flipper/hex.js
+++ 1_color-flipper/hex.js
@@ -2,6 +2,7 @@ const hex= [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 
 const btn = document.getElementById("btn");
 const color= document.querySelector(".color");
+const h4 = document.getElementsByClassName("h4");
 
 btn.addEventListener("click", ()=>{
     let hexColor = '#';
@@ -13,6 +14,11 @@ btn.addEventListener("click", ()=>{
 
     color.textContent=hexColor;
     document.body.style.backgroundColor= hexColor;
+
+      
+    for(let i=0; i< h4.length; i++){
+    h4[i].style.color = hexColor;
+    }
 })
 
 function getRandomNumber(){
